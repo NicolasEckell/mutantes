0be1f1369b15dcc531cb278e1adb14f092e8e435
Nicolas Eckell <niceckell@gmail.com>
2022-05-30 12:36:33 -0300

    refactor controller

--- app/routes/mutant.js
+++ app/routes/mutant.js
@@ -1,20 +1,9 @@
-const { isMutant } = require("../services/mutant-service.js");
+const __mutantController = require("../rest/mutant-controller.js");
 const express = require("express");
 const router = express.Router();
 
 router.post("/", async function (req, res, next) {
-    
-    const response = isMutant(req.body.dna);
-    if(response){
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.write("OK");
-        res.end();
-    }
-    else{
-        res.writeHead(403, { "Content-Type": "application/json" });
-        res.write("Forbidden");
-        res.end();
-    }
+    __mutantController.post(req, res);
 });
 
 module.exports = router;
